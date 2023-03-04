5f6cc9f3cb40138f85e1fd4879521d5c0f121a4a
James Miller <jam1211miller@gmail.com>
2023-03-04 12:11:01 -0700

    print changes

--- PrintMonitor.c
+++ PrintMonitor.c
@@ -3,6 +3,7 @@
 // 2/26/2023
 // Assignment 2 - Monitors and Semaphores
 
+// Define the _BSD_SOURCE macro and the maximum size of the printer queue
 #define _BSD_SOURCE
 #define MAX_PRINTER_QUEUE 10
 
@@ -10,127 +11,129 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-// Function prototypes
-struct Monitor *init_monitor(int);
-void add_job(struct Monitor *, int);
-int remove_job(struct Monitor *);
+// Declare the struct types for Monitor and Printer
+struct Monitor *createMonitor(int);
+void addJob(struct Monitor *, int);
+int removeJob(struct Monitor *);
 void createPrint();
 
-// Monitor definition
+// Define the Monitor struct
 struct Monitor
 {
-    struct Printer *printer;
-    int printer_queue_size;
-    int print_wait;
-    int cancel_wait;
+    struct Printer *printer; // Pointer to an array of Printer structs
+
+    int printQueueSize; // Current size of the printer queue
+    int printWait;      // Flag to indicate whether there is a job waiting to be printed
+    int cancelWait;     // Flag to indicate whether there is a job waiting to be cancelled
 };
 
-// Structure used to store the printer queue
+// Define the Printer struct
 struct Printer
 {
-    int job_id;
-    int queue_size;
+    int jobID;      // Job ID
+    int queue_size; // Size of the printer queue
 };
 
-// Initialize the monitor
-struct Monitor *init_monitor(int printer_queue_size)
-{
-    struct Monitor *monitor = (struct Monitor *)malloc(sizeof(struct Monitor));
-    monitor->printer_queue_size = printer_queue_size;
-    monitor->printer = (struct Printer *)malloc(printer_queue_size * sizeof(struct Printer));
-    monitor->print_wait = 0;
-    monitor->cancel_wait = 0;
-    return monitor;
-}
-
-// Add job to printer queue
-void add_job(struct Monitor *monitor, int job_id)
-{
-    if (monitor->printer_queue_size == MAX_PRINTER_QUEUE)
-    {
-        // Printer queue is full
-        printf("Printer queue is full, print job %d is waiting...\n", job_id);
-        monitor->print_wait = 1;
-    }
-    else
-    {
-        // Add job to printer queue
-        monitor->printer[monitor->printer_queue_size].job_id = job_id;
-        printf("Print Job %d - added to printer queue\n", job_id);
-        monitor->printer_queue_size++;
-        // If cancel job was waiting, notify it
-        if (monitor->cancel_wait)
-        {
-            monitor->cancel_wait = 0;
-            printf("Job cancellation - notified\n");
-        }
-    }
-}
-
-// Remove job from printer queue
-int remove_job(struct Monitor *monitor)
-{
-    if (monitor->printer_queue_size == 0)
-    {
-        // Printer queue is empty
-        printf("Printer queue is empty, job is waiting...\n");
-        monitor->cancel_wait = 1;
-        return -1;
-    }
-    else
-    {
-        // Remove job from printer queue
-        int job_id = monitor->printer[monitor->printer_queue_size - 1].job_id;
-        printf("Job %d - removed from printer queue\n", job_id);
-        monitor->printer_queue_size--;
-        // If print job was waiting, notify it
-        if (monitor->print_wait)
-        {
-            monitor->print_wait = 0;
-            printf("Print job - notified\n");
-        }
-        return job_id;
-    }
-}
-
+// Function to print a message indicating the producer-consumer scenario
 void createPrint()
 {
-    printf("Job Producer ID 1\n");
-    printf("Job Consumer ID 2\n");
+    printf("Job -> Producer ID 1\n");
+    printf("Job -> Consumer ID 2\n");
+}
+
+// Function to create and initialize a monitor struct.
+struct Monitor *createMonitor(int printQueueSize)
+{
+    struct Monitor *monitor = (struct Monitor *)malloc(sizeof(struct Monitor));           // Allocate memory for the Monitor struct
+    monitor->printQueueSize = printQueueSize;                                             // Initialize the print queue size
+    monitor->printer = (struct Printer *)malloc(printQueueSize * sizeof(struct Printer)); // Allocate memory for the Printer structs
+
+    monitor->printWait = 0;  // Initialize the print wait flag
+    monitor->cancelWait = 0; // Initialize the cancel wait flag
+    return monitor;          // Return the initialized Monitor struct
+}
+
+// Function to add a print job to the printer queue
+void addJob(struct Monitor *monitor, int jobID)
+{
+    // Check if the printer queue is full
+    if (monitor->printQueueSize != MAX_PRINTER_QUEUE)
+    {
+        // Add the print job to the printer queue
+        monitor->printer[monitor->printQueueSize].jobID = jobID;
+        printf("Print Job %d - added to queue printer \n", jobID);
+        monitor->printQueueSize++; // Increment the print queue size
+        // Check if there is a job waiting to be cancelled
+        if (monitor->cancelWait)
+        {
+            monitor->cancelWait = 0; // Clear the cancel wait flag
+            printf("Print Job cancellation - notified\n");
+        }
+    }
+    else
+    {
+        printf("Queue for printer is full, print job %d is waiting...\n", jobID);
+        monitor->printWait = 1; // Set the print wait flag
+    }
+}
+
+// Function to remove a print job from the printer queue
+int removeJob(struct Monitor *monitor)
+{
+    // Check if the printer queue is empty
+    if (monitor->printQueueSize != 0)
+    {
+        // Remove a print job from the printer queue
+        int jobID = monitor->printer[monitor->printQueueSize - 1].jobID;
+        printf("Print Job %d - removed from the printer queue\n", jobID);
+        monitor->printQueueSize--; // Decrement the print queue size
+        // Check if there is a job waiting to be printed
+        if (monitor->printWait)
+        {
+            monitor->printWait = 0;
+            printf("Print job - notified\n");
+        }
+        return jobID;
+    }
+    else
+    {
+
+        printf("Printer queue is empty, job is waiting...\n");
+        monitor->cancelWait = 1; // Set the cancel wait flag
+        return -1;
+    }
 }
 
 int main()
 {
-    int printer_queue_size = 0;
-    struct Monitor *monitor = init_monitor(printer_queue_size);
+    int amount = 0;
+    struct Monitor *monitor = createMonitor(amount);
 
-    printf("Creating print jobs (Producer Consumer Scenario).\n");
+    printf("Creating print jobs - (Producer Consumer Scenario).\n");
     createPrint();
-    printf("\n");
+    printf("-----------------------\n");
 
     int i = 5;
 
     while (i > 0)
     {
-        // Add 3 jobs to printer queue
-        for (int i = 1; i <= 3; i++)
+        for (int i = 1; i <= 4; i++)
         {
-            add_job(monitor, i);
-            usleep(500000);
+            addJob(monitor, i);
+            usleep(100000);
         }
 
-        // Remove 3 jobs from printer queue
-        for (int i = 1; i <= 3; i++)
+        for (int i = 1; i <= 4; i++)
         {
-            int job_id = remove_job(monitor);
-            if (job_id != -1)
+            int jobID = removeJob(monitor);
+            if (jobID != -1)
             {
-                // printf("Job %d printed.\n", job_id);
             }
-            usleep(500000);
+            usleep(100000);
         }
         i--;
     }
-
+    printf("-----------------------\n");
+    printf("Ending -> Producer Consumer Scenario\n");
     return 0;
 }
